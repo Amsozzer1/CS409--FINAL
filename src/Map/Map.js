5a6a5b72ec9eeeeb6c291026a88db84916ff01e3
sunwangzhe <wangzhe2@illinois.edu>
2023-11-25 18:18:50 -0600

    add Map Component

---
+++ src/Map/Map.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { GoogleMap, useLoadScript, MarkerF } from '@react-google-maps/api';
+import { useState, useEffect } from 'react';
+
+import Navbar from '../Navbar/navbar';
+
+const libraries = ['places'];
+const mapContainerStyle = {
+  width: '100%',
+  height: '100%',
+};
+
+const Map = () => {
+  const [center, setCenter] = useState({ lat: 40.110558, lng: -88.228333 });
+  const [zoom, setZoom] = useState(16);
+
+
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: 'AIzaSyCG8MUFrbUkfNNxhg-gcs-DM5Rku9pSsHM',
+    libraries,
+  });
+
+  if (loadError) {
+    return <div>Error loading maps</div>;
+  }
+
+  if (!isLoaded) {
+    return <div>Loading maps</div>;
+  }
+
+  const handleOnClick = (event) => {
+    const clickedLat = event.latLng.lat();
+    const clickedLng = event.latLng.lng();
+
+    // console.log(`Clicked on: Lat ${clickedLat}, Lng ${clickedLng}`);
+    setCenter({ lat:clickedLat, lng:clickedLng });
+  }
+
+  return (
+    <div>
+      <Navbar />
+      <div style={{ top: '80px', height: 'calc(100vh - 80px)', width: '100%' }}>
+        <GoogleMap
+          mapContainerStyle={mapContainerStyle}
+          zoom={zoom}
+          center={center}
+          onClick={handleOnClick}
+          options={{
+            fullscreenControl: false,
+            streetViewControl: false,
+            mapTypeControl: false,
+          }}
+        >
+          <MarkerF position={center} />
+        </GoogleMap>
+      </div>
+    </div>
+  );
+};
+
+export default Map;
