c9c2a64a354ba73b8da967ad41a0365135f689c0
jenny <jennysun0830@gmail.com>
2023-11-28 11:48:19 -0600

    add event, time

--- src/AdvanceSearch/advsearch.js
+++ src/AdvanceSearch/advsearch.js
@@ -1,8 +1,14 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { TimePicker } from '@mui/x-date-pickers/TimePicker';
 import { Button } from '@mui/material';
 import {Input} from '@mui/material';
 import {Search} from '@mui/icons-material';
+import { useState } from 'react';
+import { Select, MenuItem } from '@mui/material';
 import Results from '../AdvanceSearch/Results.js';
 import '../index.css';
 
@@ -30,11 +36,15 @@ const data = [
     the search query?)
 */
 
-
 export default function AdvSearch(){
 
     const [open, setOpen] = React.useState(false);
+    const [selectedValue, setSelectedValue] = useState('');
+    const [selectedTime, setSelectedTime] = React.useState(null);
 
+    const handleChange = (event) => {
+        setSelectedValue(event.target.value);
+    }
 
     return (
         <Box
@@ -100,6 +110,59 @@ export default function AdvSearch(){
                     justifyContent: 'center',
                 }}
                 >
+                <Select
+                    id="my-dropdown"
+                    value={selectedValue}
+                    onChange={handleChange}
+                    displayEmpty
+                    inputProps={{ 'aria-label': 'Without label' }}
+                    sx={{
+                        width: '200px',
+                        height: '30px',
+                        borderRadius: '5px',
+                        backgroundColor: 'white',
+                        opacity: '0.9',
+                        color: 'black',
+                        marginLeft: '5px',
+                        marginTop: '20px',
+                        '& .MuiSelect-select': {
+                        paddingTop: '5px',
+                        paddingBottom: '5px',
+                        },
+                    }}
+                    >
+                    <MenuItem value="">
+                        <em>Event in next two days</em>
+                    </MenuItem>
+                    <MenuItem value="destination1">Event 1</MenuItem>
+                    <MenuItem value="destination2">Event 2</MenuItem>
+                    <MenuItem value="destination3">Event 3</MenuItem>
+                </Select>
+                
+                <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <TimePicker
+                    value={selectedTime}
+                    onChange={(newValue) => {
+                    setSelectedTime(newValue);
+                    }}
+                    renderInput={(params) => <TextField {...params} 
+                    sx={{
+                        width: '200px',
+                        height: '40px', // 调整高度以适应内边距
+                        borderRadius: '5px',
+                        backgroundColor: 'rgba(255, 255, 255)', // 白色背景带透明度
+                        color: 'black',
+                        marginLeft: '5px',
+                        marginTop: '20px',
+                        '& .MuiOutlinedInput-input': {
+                          paddingTop: '10px',
+                          paddingBottom: '10px',
+                        }
+                      }}
+                    />}
+                />
+                </LocalizationProvider>
+
                 <Input
                     type='text'
                     placeholder='Destination'
@@ -116,39 +179,7 @@ export default function AdvSearch(){
 
                     }}
                     ></Input>
-                    <Input
-                    type='text'
-                    placeholder='Destination'
-                    sx={{
-                        width: '200px',
-                        height: '30px',
-                        borderRadius: '5px',
-                        backgroundColor: 'white',
-                        opacity: '0.9',
-                        color: 'black',
-                        marginLeft: '5px',
-                        marginTop: '20px',
-                    verticalAlign: 'middle',
-
-                    }}
-                    ></Input>
-                    <Input
-                    type='text'
-                    placeholder='Destination'
-                    sx={{
-                        width: '200px',
-                        height: '30px',
-                        borderRadius: '5px',
-                        backgroundColor: 'white',
-                        opacity: '0.9',
-                        color: 'black',
-                        marginLeft: '5px',
-                        marginTop: '20px',
-                    verticalAlign: 'middle',
-
-                    }}
-                    ></Input>
-                    <Input
+                <Input
                     type='text'
                     placeholder='Destination'
                     sx={{
