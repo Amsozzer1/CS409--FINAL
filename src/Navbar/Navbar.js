4439b9871ba301427d13f63c5f1b63490e72be55
Jason717717 <1354471825@qq.com>
2023-12-07 17:55:09 -0600

    fix when user has no events

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -77,7 +77,7 @@ export default function Navbar(){
               // User not found in the database
               if (response.status === 404) {
                 console.log('User not found in the database');
-                return [];
+                return {events: []};
               } else {
                 // Handle other non-success status codes
                 console.error(`Error fetching events: ${response.status}`);
@@ -86,6 +86,7 @@ export default function Navbar(){
             return response.json();
           })
           .then(data => {
+            sessionStorage.setItem(userId, JSON.stringify(data.events));
             const events = data.events.map((event) => {
               return {
                 ...event,
