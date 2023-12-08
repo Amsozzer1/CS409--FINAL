858b43ec13f44b90afe33b53c2cf7271f1ca4520
jenny <jennysun0830@gmail.com>
2023-12-07 20:22:30 -0600

    combined adv search

--- src/AdvanceSearch/advsearch.js
+++ src/AdvanceSearch/advsearch.js
@@ -1,53 +1,74 @@
+/* eslint-disable no-undef */
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { TimePicker } from '@mui/x-date-pickers/TimePicker';
 import { Button } from '@mui/material';
 import {Input} from '@mui/material';
 import {Search} from '@mui/icons-material';
-import { useState } from 'react';
+// <<<<<<< wangzhe
+import Results from './Results.js';
+import {useLoadScript,Autocomplete,DirectionsRenderer } from '@react-google-maps/api';
+// import { DESTINATION } from '../Map/Map.js';
+
+import { useState, useEffect, useRef } from 'react';
+
+// =======
+// import Results from '../AdvanceSearch/Results.js';
+// import {useLoadScript,Autocomplete } from '@react-google-maps/api';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { TimePicker } from '@mui/x-date-pickers/TimePicker';
+import TextField from '@mui/material/TextField';
+// import { useState } from 'react';
 import { Select, MenuItem } from '@mui/material';
 import IconButton from '@mui/material/IconButton';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
-import Results from '../AdvanceSearch/Results.js';
+// >>>>>>> main
 import '../index.css';
 import { v4 as uuidv4 } from 'uuid';
 import Typography from '@mui/material/Typography';
+import '../index.css';
 import { useUser } from '../User/User.js';
+const libraries = ['places'];
 
+export var ROUTE = '';
 /* CHANGE DATA TO UPDATE RESULTS AND ALSO CHANGE AVATAR
     FROM Results.js*/
-const data = [
-    {
-        id: 1,
-        name: 'Hawaii',
-        email: 'abc@gmail.com',
-        sirName: 'Hawaii',
-
-    }
+// const data = [
     
-  ];
+//     {
+//         id: 1,
+//         name: 'Hawaii',
+//         email: 'abc@gmail.com',
+//         sirName: 'Hawaii',
+
+//     }
+    
+//   ];
 /* CHANGE DATA TO UPDATE RESULTS (fecth from backend based on
     the search query?)
 */
-export var ROUTE = '';
 
-export default function AdvSearch(){
 
-    const [open, setOpen] = React.useState(false);
-    const [selectedValue, setSelectedValue] = useState('');
-    const [selectedTime, setSelectedTime] = React.useState(null);
+export var BUS = '';
+
+
+
+export default function AdvSearch(props){
+    const sendData = (mes)=>{
+        props.parentCallback(mes);
+    };
+
+// <<<<<<< wangzhe
+
     const {user} = useUser();
     const events = user.getEvents();
-    console.log( events );
-
     const handleChange = (event) => {
         setSelectedValue(event.target.value);
     }
 
+    const [selectedValue, setSelectedValue] = useState('');
+    const [selectedTime, setSelectedTime] = React.useState(null);
     const [destinations, setDestinations] = useState([{ id: uuidv4(), name: '' }]);
 
     const addDestination = () => {
@@ -56,19 +77,212 @@ export default function AdvSearch(){
 
     const removeDestination = (id) => {
         setDestinations(destinations.filter(dest => dest.id !== id));
-      };
+        };
 
     const handleDestinationChange = (id, newValue) => {
         const newDestinations = destinations.map(dest => {
-          if (dest.id === id) {
+            if (dest.id === id) {
             return { ...dest, name: newValue };
-          }
-          return dest;
+            }
+            return dest;
         });
         setDestinations(newDestinations);
-      };
+        };
+
+
+
+    
+
+    const [open, setOpen] = useState(false);
+    // const [center, setCenter] = useState({ lat: 40.110558, lng: -88.228333 });
+    // const [directionResponse, setDirectionResponse] = useState(null);
+    // const [distance, setDistance] = useState(0);
+    // const [duration, setDuration] = useState(0);
+    // const [long, setLong] = useState(-88.22884);
+    // const [lat, setLat] = useState(40.11644);
+    
+
+    // const [data, setData] = useState([]);
+   
+
+    const [origin, setOrigin] = useState({lat: 40.12233, lon: -88.29619});
+    const [destination, setDestination] = useState({lat: 40.11626, lon: -88.25783});
+
+    const [itinerary, setItinerary] = useState(null);
+
+    const [walkTrip, setWalkTrip] = useState([]);
+    const [busTrip, setBusTrip] = useState([]);
+
+    const [walkTripInfo, setWalkTripInfo] = useState([]);
+    const [busTripInfo, setBusTripInfo] = useState([]);
+
+    const [routeShape, setRouteShape] = useState(null);
+
+    const [currRoute, setCurrRoute] = useState("");
+    const [vehicles, setVehicles] = useState([]);
+
+
+    async function getPlannedTrip() {
+        const URL = `https://developer.mtd.org/api/v2.2/json/getplannedtripsbylatlon?key=ca74c75b34e64cc9bde55c9714918493&origin_lat=${origin.lat}&origin_lon=${origin.lon}&destination_lat=${destination.lat}&destination_lon=${destination.lon}`;
+      
+        try {
+            const response = await fetch(URL);
+            const data = await response.json();
+            const currItinerary = data.itineraries[0];
+
+
+            const trips = currItinerary.legs;
+
+            // console.log(trips);
+
+            let walkTrips = []
+            let busTrips = [];
+            if (trips !== null) {
+                for (let i = 0; i < trips.length; ++i) {
+                    let trip = trips[i];
+
+                    if (trip.type === 'Walk') {
+                        walkTrips.push(trip);
+                    } else if (trip.type === 'Service') {
+                        busTrips.push(trip)
+                    }
+                }
+
+            }
+
+            setItinerary(data.itineraries[0]);
+            setWalkTrip(walkTrips);
+            setBusTrip(busTrips);
+            
+            console.log(walkTrip);
+            // console.log(busTrip);
+
+
+          } catch (error) {
+            console.error('Error fetching stop data: ', error);
+          }
+    }
+
+    async function getWalkInfo() {
+        let walkInfo = [];
+        let walkResult = [];
+        for (let i = 0; i < walkTrip.length; ++i) {
+            let walk = {};
+            walk.origin = walkTrip[i].walk.begin;
+            walk.destnation = walkTrip[i].walk.end;
+
+            walkInfo.push(walk);
+
+            const directionsService = new google.maps.DirectionsService();
+            const results = await directionsService.route({
+                origin: { lat: walk.origin.lat, lng: walk.origin.lon },
+                destination: { lat: walk.destnation.lat, lng: walk.destnation.lon },
+                travelMode: google.maps.TravelMode.WALKING,   
+            });
+
+            walkResult.push(results);
+        }
+
+        // console.log(walkResult);
+
+        setWalkTripInfo(walkResult);
+        console.log(walkTripInfo);
+    }
+
+    async function getBusInfo() {
+        let busInfo = [];
+        for (let i = 0; i < busTrip.length; ++i) {
+            let bus = {};
+            let service = busTrip[i].services[0]
+
+            const URL = `https://developer.mtd.org/api/v2.2/json/getshape?key=ca74c75b34e64cc9bde55c9714918493&shape_id=${service.trip.shape_id}`;
+            try {
+                const response = await fetch(URL);
+                const data = await response.json();
+
+                bus.shape = data.shapes;
+            } catch (error) {
+                console.error('Error fetching stop data: ', error);
+            }
+
+
+
+            bus.origin = service.begin.stop_id;
+            bus.destination = service.end.stop_id;
+            bus.route = service.route.route_id;
+            // bus.shape = service.trip.shape_id;
+            bus.trip = service.trip.trip_id;
+
+            busInfo.push(bus);
+        }
+
+        setBusTripInfo(busInfo);
+
+        // console.log(busTripInfo);
+    }
+
+    async function getVehicles() {
+        const vehicleURL = `https://developer.mtd.org/api/v2.2/json/getvehiclesbyroute?key=ca74c75b34e64cc9bde55c9714918493&route_id=${currRoute}`;
+      
+        try {
+            const response = await fetch(vehicleURL);
+            const data = await response.json();
+
+            // console.log(data);
+
+            const currVehicles = data.vehicles;
+            
+
+            setVehicles(currVehicles);
+            
+            // console.log(vehicles);
+
+
+        } catch (error) {
+            console.error('Error fetching stop data: ', error);
+        }
+    }
+
+
+
+    const { isLoaded, loadError } = useLoadScript({
+        googleMapsApiKey: 'AIzaSyCG8MUFrbUkfNNxhg-gcs-DM5Rku9pSsHM',
+        libraries,
+      });
+    
+      if (loadError) {
+        return <div>Error loading maps</div>;
+      }
+    
+      if (!isLoaded) {
+        return <div>Loading maps</div>;
+      }
+
+
+
+    function handleSearch() {
+        getPlannedTrip();
+
+        getWalkInfo();
+        getBusInfo();
+
+        if (busTripInfo.length > 0) {
+            // console.log(busTripInfo);
+            setCurrRoute(busTripInfo[0].route);
+        }
+
+        if (currRoute !== "") {
+            getVehicles();
+        }
+        
+
+
+        console.log(itinerary);
+    }
 
     return (
+        // <div style={{ position: 'relative', zIndex: 2 }}>
+        // <div>
         <Box
         sx={{
             position: 'absolute',
@@ -91,7 +305,8 @@ export default function AdvSearch(){
                 sx={{
                     color: 'black',
                 }}
-                >  
+                >
+                    
                         <input type='checkbox'
                         className='checkbox-round'
                         onClick={()=>{
@@ -103,8 +318,14 @@ export default function AdvSearch(){
                             {
                                 setOpen(false);
                             }
+            
+
                         }}
-                    ></input>           
+                        
+                        
+                    
+                    ></input>
+                    
                     <p>AdvanceSearch</p>
                 </Button>
                 <Button>
@@ -112,6 +333,9 @@ export default function AdvSearch(){
                     sx={{
                     color: '#E84A27',
                     verticalAlign: 'middle',
+                    
+
+
                     }}/>
                 </Button>
                 <Box
@@ -144,8 +368,8 @@ export default function AdvSearch(){
                         <em>Event in next two days</em>
                     </MenuItem>
                     {events.map( (event, index) => (
-                        <MenuItem key = {index} value = {event.title}>
-                            {event.title}
+                        <MenuItem key = {index} value = {EventSource.name}>
+                            {EventSource.name}
                         </MenuItem>
                     ))}
                 </Select>
@@ -204,6 +428,7 @@ export default function AdvSearch(){
 
                 {destinations.map((destination, index) => (
                     <Box key={destination.id} sx={{ display: 'flex', alignItems: 'center' }}>
+                    <Autocomplete>
                     <TextField
                     key={destination.id}
                     value={destination.name}
@@ -221,6 +446,7 @@ export default function AdvSearch(){
                         marginTop: '10px',  
                         }}
                     />
+                    </Autocomplete>
                     <IconButton onClick={() => removeDestination(destination.id)} color="error" aria-label="remove stop">
                         <RemoveCircleOutlineIcon />
                     </IconButton>
@@ -240,6 +466,7 @@ export default function AdvSearch(){
                 </Typography>
                 </Box>
                 </Box>
+
             </Box>
             : //else
             <Box
@@ -254,7 +481,7 @@ export default function AdvSearch(){
                 sx={{
                     color: 'black',
                 }}
-                >  
+                >                   
                         <input type='checkbox'
                         className='checkbox-round'
                         onClick={()=>{
@@ -266,26 +493,24 @@ export default function AdvSearch(){
                             {
                                 setOpen(false);
                             }
-                        }}
-                    ></input>           
-                    <p>AdvanceSearch</p>
-                </Button>
-                <Button>
-                <Search
-                    sx={{
-                    color: '#E84A27',
-                    verticalAlign: 'middle',
-                    }}/>
+                        }}                    
+                    ></input>
+                    
+                    <p>Advanced Search</p>
+
+                    
                 </Button>
                 <Box
                 sx={{
                     display: 'flex',
-                    flexDirection: 'column',
+                    flexDirection: 'row',
                     marginLeft: '10px'
                 }}
                 >
+                <Autocomplete>
                 <Input
                     type='text'
+                    name='destination'
                     placeholder='Destination'
                     sx={{
                         width: '213.171px',
@@ -298,13 +523,33 @@ export default function AdvSearch(){
                         marginTop: '10px',
                         marginBottom: '20px'
                     }}
+                    onChange={(event)=>{
+
+                        console.log(event.target.value);
+
+                    }}
                     ></Input>
+                </Autocomplete>
+                    <Button
+                    onClick={handleSearch}
+                    >
+                    <Search
+                    sx={{
+                    color: '#E84A27',
+                    verticalAlign: 'middle',
+                    marginLeft: '-25px',
+                    marginTop: '20px',
+                    }}/>
+                    </Button>
                 </Box>
                 
             </Box>
             }
-            <Results data={data}></Results>
+            
+            {/* <Results data={data}></Results> */}
+            
             
         </Box>
+        // </div>
     );
-}
+};
